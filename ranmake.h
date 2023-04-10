ae233acf2bf279770aa3e28e4b3269c4c9caf122
Ransel117 <jonatanronnberg@gmail.com>
2023-04-10 02:56:55 +0200

    renamed repo

--- ranmake.h
+++ ranmake.h
@@ -21,33 +21,37 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
 */
-#ifndef RAKE_H
+#ifndef RANMAKE_H
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-#define RAKE_VER "0.0.1"
+#define RANMAKE_VER "0.0.1"
 
-#define RAKE_VALID_ARGS_AMOUNT 6
-#define RAKE_VER_ARG1  "-v"
-#define RAKE_VER_ARG2  "--version"
-#define RAKE_HELP_ARG1 "-h"
-#define RAKE_HELP_ARG2 "--help"
-#define RAKE_F_ARG1 "-f"
-#define RAKE_F_ARG2 "--file"
-#define RAKE_DEF_F1 "rake"
-#define RAKE_DEF_F2 "__rake"
+#define RANMAKE_VALID_ARGS_AMOUNT 6
+#define RANMAKE_VER_ARG1  "-v"
+#define RANMAKE_VER_ARG2  "--version"
+#define RANMAKE_HELP_ARG1 "-h"
+#define RANMAKE_HELP_ARG2 "--help"
+#define RANMAKE_F_ARG1 "-f"
+#define RANMAKE_F_ARG2 "--file"
+#define RANMAKE_DEF_F1 "ranmakefile"
+#define RANMAKE_DEF_F2 "__ranmakefile"
 
-#define RAKE_READ_CHUNK_SIZE 2097152
-#define RAKE_READ_ERR_G "Error reading file: %s. errno: %d"
-#define RAKE_READ_ERR_MEM "Not enough free memory to read file: %s"
+#define RANMAKE_HELP_MSG "-v, --version    prints the version of the software\n" \
+          "-h, --help       prints this help message\n"                          \
+          "-f, --file       uses a specified file instead of the default "RANMAKE_DEF_F1" or "RANMAKE_DEF_F2"\n"
+
+#define RANMAKE_READ_CHUNK_SIZE 2097152
+#define RANMAKE_READ_ERR_G "Error reading file: %s. errno: %d"
+#define RANMAKE_READ_ERR_MEM "Not enough free memory to read file: %s"
 
 typedef enum {
-    RAKE_SUCCESS,
-    RAKE_ERR,
-    RAKE_ERR_F_N_FND,
-} rake_error;
+    RANMAKE_SUCCESS,
+    RANMAKE_ERR,
+    RANMAKE_ERR_F_N_FND,
+} ranmake_error;
 
 typedef struct {
     char *data;
@@ -55,49 +59,47 @@ typedef struct {
     bool is_valid;
 } File;
 
-#define RAKE_ERR_RET(R, ...) do { \
+#define RANMAKE_ERR_RET(R, ...) do { \
     fprintf(stderr, __VA_ARGS__);      \
     fprintf(stderr, "\n");             \
     return R;                          \
 } while(0);
-#define RAKE_ERR_EXIT(...) do { \
+#define RANMAKE_ERR_EXIT(...) do { \
     fprintf(stderr, __VA_ARGS__); \
     fprintf(stderr, "\n");        \
     exit(1);                      \
 } while(0);
 
 
-int32_t rake_print_ver(void);
-int32_t rake_print_help(void);
-bool rake_str_eq(const char*, const char*);
-bool rake_file_exists(const char*);
-void rake_print_args(const char**, const uint32_t);
-File *rake_read_file(const char*);
-int32_t rake_main(const char*);
+int32_t ranmake_print_ver(void);
+int32_t ranmake_print_help(void);
+bool ranmake_str_eq(const char*, const char*);
+bool ranmake_file_exists(const char*);
+void ranmake_print_args(const char**, const uint32_t);
+File *ranmake_read_file(const char*);
+int32_t ranmake_main(const char*);
 
-#endif /* RAKE_H */
+#endif /* RANMAKE_H */
 
-#ifdef RAKE_IMPLEMENTATION
+#ifdef RANMAKE_IMPLEMENTATION
 #include <stdio.h>
 #include <string.h>
 #include <malloc.h>
 #include <errno.h>
 
-int32_t rake_print_ver(void) {
-    return printf("Rake "RAKE_VER"\n");
+int32_t ranmake_print_ver(void) {
+    return printf("Ranmake "RANMAKE_VER"\n");
 }
 
-int32_t rake_print_help(void) {
-    return printf("-v, --version    prints the version of the software\n"
-                  "-h, --help       prints this help message\n"
-                  "-f, --file       uses a specified file instead of the default rake or __rake\n");
+int32_t ranmake_print_help(void) {
+    return printf(RANMAKE_HELP_MSG);
 }
 
-bool rake_str_eq(const char *a, const char *b) {
+bool ranmake_str_eq(const char *a, const char *b) {
     return strcmp(a, b) == 0;
 }
 
-bool rake_file_exists(const char *file) {
+bool ranmake_file_exists(const char *file) {
     FILE *f;
 
     f = fopen(file, "r");
@@ -108,7 +110,7 @@ bool rake_file_exists(const char *file) {
     return false;
 }
 
-void rake_print_args(const char **args, const uint32_t count) {
+void ranmake_print_args(const char **args, const uint32_t count) {
     uint32_t i;
 
     for (i = 1; i < count; ++i) {
@@ -116,7 +118,7 @@ void rake_print_args(const char **args, const uint32_t count) {
     }
 }
 
-File *rake_read_file(const char *path) {
+File *ranmake_read_file(const char *path) {
     char *data, *tmp;
     size_t used, size, n;
     File *file;
@@ -128,32 +130,32 @@ File *rake_read_file(const char *path) {
 
     file = (File*)malloc(sizeof(File));
     if (!file) {
-        RAKE_ERR_RET(file, "Unable to allocate memory");
+        RANMAKE_ERR_RET(file, "Unable to allocate memory");
     }
     file->is_valid = false;
 
     fp = fopen(path, "r");
     if (!fp || ferror(fp)) {
-        RAKE_ERR_RET(file, RAKE_READ_ERR_G, path, errno);
+        RANMAKE_ERR_RET(file, RANMAKE_READ_ERR_G, path, errno);
     }
 
     while (true) {
-        if (used + RAKE_READ_CHUNK_SIZE + 1 > size) {
-            size = used + RAKE_READ_CHUNK_SIZE + 1;
+        if (used + RANMAKE_READ_CHUNK_SIZE + 1 > size) {
+            size = used + RANMAKE_READ_CHUNK_SIZE + 1;
             if (size <= used) {
                 free(data);
-                RAKE_ERR_RET(file, "Input file too large: %s", path);
+                RANMAKE_ERR_RET(file, "Input file too large: %s", path);
             }
 
             tmp = realloc(data, size);
             if (!tmp) {
                 free(data);
-                RAKE_ERR_RET(file, RAKE_READ_ERR_MEM, path);
+                RANMAKE_ERR_RET(file, RANMAKE_READ_ERR_MEM, path);
             }
             data = tmp;
         }
 
-        n = fread(data + used, 1, RAKE_READ_CHUNK_SIZE, fp);
+        n = fread(data + used, 1, RANMAKE_READ_CHUNK_SIZE, fp);
         if (n == 0) {
             break;
         }
@@ -163,13 +165,13 @@ File *rake_read_file(const char *path) {
 
     if (ferror(fp)) {
         free(data);
-        RAKE_ERR_RET(file, RAKE_READ_ERR_G, path, errno);
+        RANMAKE_ERR_RET(file, RANMAKE_READ_ERR_G, path, errno);
     }
 
     tmp = realloc(data, used + 1);
     if (!tmp) {
         free(data);
-        RAKE_ERR_RET(file, RAKE_READ_ERR_MEM, path);
+        RANMAKE_ERR_RET(file, RANMAKE_READ_ERR_MEM, path);
     }
 
     data = tmp;
@@ -187,7 +189,7 @@ char *get_value(const char *config_buffer, const char *value) {
 
     line = strstr(config_buffer, value);
     if (!line) {
-        RAKE_ERR_EXIT("Could not find value: %s.\n", value);
+        RANMAKE_ERR_EXIT("Could not find value: %s.\n", value);
     }
 
     size_t len;
@@ -217,7 +219,7 @@ void dostuff(const char *path) {
     File *file;
     char *compiler, *flags, *defines, *includes, *files, *libs, *exec, *output, *cmd;
 
-    file = rake_read_file(path);
+    file = ranmake_read_file(path);
 
     compiler = get_value(file->data, "CC");
     flags    = get_value(file->data, "FLAGS");
@@ -226,7 +228,7 @@ void dostuff(const char *path) {
     files    = get_value(file->data, "FILES");
     libs     = get_value(file->data, "LIBS");
     exec     = get_value(file->data, "EXEC");
-    output = rake_str_eq(compiler, "cl") || rake_str_eq(compiler, "CL") || rake_str_eq(compiler, "clang-cl") ? "/OUT:" : "-o";
+    output = ranmake_str_eq(compiler, "cl") || ranmake_str_eq(compiler, "CL") || ranmake_str_eq(compiler, "clang-cl") ? "/OUT:" : "-o";
     cmd = (char*)malloc(sizeof(char*)*8);
 
     snprintf(cmd, sizeof(cmd)*8, "%s %s %s %s %s %s %s %s", compiler, flags, defines, includes, files, libs, output, exec);
@@ -236,23 +238,23 @@ void dostuff(const char *path) {
     system(cmd);
 }
 
-int32_t rake_main(const char *path) {
-    if (rake_str_eq(path, "")) {
-        if (rake_file_exists(RAKE_DEF_F1)) {
-            dostuff(RAKE_DEF_F1);
-        } else if (rake_file_exists(RAKE_DEF_F2)) {
-            dostuff(RAKE_DEF_F2);
+int32_t ranmake_main(const char *path) {
+    if (ranmake_str_eq(path, "")) {
+        if (ranmake_file_exists(RANMAKE_DEF_F1)) {
+            dostuff(RANMAKE_DEF_F1);
+        } else if (ranmake_file_exists(RANMAKE_DEF_F2)) {
+            dostuff(RANMAKE_DEF_F2);
         } else {
-            RAKE_ERR_RET(RAKE_ERR_F_N_FND, "Could not find the default file: "RAKE_DEF_F1" or "RAKE_DEF_F2);
+            RANMAKE_ERR_RET(RANMAKE_ERR_F_N_FND, "Could not find the default file: "RANMAKE_DEF_F1" or "RANMAKE_DEF_F2);
         }
     } else {
-        if (!rake_file_exists(path)) {
-            RAKE_ERR_RET(RAKE_ERR_F_N_FND, "Could not find the file: %s", path);
+        if (!ranmake_file_exists(path)) {
+            RANMAKE_ERR_RET(RANMAKE_ERR_F_N_FND, "Could not find the file: %s", path);
         }
         dostuff(path);
     }
 
-    return RAKE_SUCCESS;
+    return RANMAKE_SUCCESS;
 }
 
-#endif /* RAKE_IMPLEMENTATION */
+#endif /* RANMAKE_IMPLEMENTATION */
